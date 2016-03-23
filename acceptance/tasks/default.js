09e0818a6853ff44e47c9e7394a10a4f9486e923
Dariusz Lorenc <dlorenc@pivotal.io>
2016-03-23 17:10:25 +1100

    integrate gradle with gulp build

--- acceptance/tasks/default.js
+++ acceptance/tasks/default.js
@@ -2,3 +2,6 @@ const gulp = require('gulp');
 const runSequence = require('run-sequence');
 
 gulp.task('default', cb => runSequence('spec', cb));
+
+//to bind default gradle plugin's task (gulp_build) to our default gulp task (spec)
+gulp.task('build', cb => runSequence('default', cb));
